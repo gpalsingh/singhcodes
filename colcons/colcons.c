fee47a2f3774241e288c941262c482ea60711cac
Gurkirpal Singh <gurkirpal204@gmail.com>
2015-11-20 19:43:05 +0530

    fixing double function

--- colcons/colcons.c
+++ colcons/colcons.c
@@ -6,6 +6,8 @@
 int __colx__ = 0;
 int __coly__ = 0;
 
+void putcolChar( char c );
+
 void resetColoredConsole(){
     cleardevice();
     __coly__ = 0;
@@ -13,6 +15,7 @@ void resetColoredConsole(){
 }
 
 void Enter(){
+    putcolChar('\0');
     __colx__ = 0;
     __coly__ += textwidth("k");
     
@@ -25,14 +28,14 @@ void moveCursor(){
     __colx__ += textheight("k");
     
     //can't draw too far to the right
-    if( __colx__ >= __max_colx__ ){
+    if( __colx__ == __max_colx__-3 ){
         Enter();
     }
     
 }
 
 void putcolChar( char c ){
-    static char t[2] = {' ', '\0'};  //avoided calls to sprintf
+    static char t[2] = {' ', '\0'}; 
     t[0] = c;
     outtextxy(__colx__, __coly__, t);
     moveCursor();
@@ -44,6 +47,46 @@ void putString( char* st ){
     for(i=0; st[i]!='\0'; i++){
         putcolChar( st[i] );
     }
+}
+
+char* doubleToString( int a ){
+    static char t[50], sign=' ';
+    int i,j;
+    double mantissa = a%1;
+    
+    if(a < 0){
+        sign = '-';
+        a *= 1.0;
+    }
+    
+    a -= mantissa;
+    
+    for(i=49; a>1; i--, a/=10.0){
+        t[i] = '0' + (int)(a%10);
+    }
+    
+    if(sign == '-'){
+        t[0] = sign;
+        j = 1;
+    }else{
+        j = 0;
+    }
+    
+    for(++i; i<50; i++, j++){
+        t[j] = t[i];
+    }
+    
+    t[j] = '.';
+    
+    for(++j, i=0; i<4; i++, j++){
+        mantissa *= 10.0;
+        t[j] = (int)mantissa;
+        mantissa = mantissa % 1;
+    }
+    
+    t[j] = '\0';
+    
+    return t;
 }
 
 char* intToString( int a ){
@@ -70,9 +113,7 @@ char* intToString( int a ){
     }
     
     //put number in front
-    i++;
-    
-    for(; (i<20) && (j<20); i++, j++){
+    for(++i; (i<20) && (j<20); i++, j++){
         t[j] = t[i];
     }
     //make a valid string
@@ -118,8 +159,7 @@ void colprintf(char *fmt, ...){
                 break;
                 
             case 'f':   //float, similar to above
-                sprintf(temp, "%f", va_arg(li, double));
-                putString( temp );
+                putString( doubleToString(va_arg(li, double)) );
                 break;
                 
             case 's':   //string
